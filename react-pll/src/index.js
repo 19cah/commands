a76ed66dd372ee0ad5941431ffa94134f0f4ab6d
Abraham Hernandez <abraham@abranhe.com>
2020-01-01 16:23:57 -0500

    finishing react-pll-demo

--- react-pll/src/index.js
+++ react-pll/src/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactPll from 'react-pll';
 import './index.css';
-import App from './App';
 
+const languages = [ 'c', 'cpp', 'csharp', 'css', 'go_old', 'go', 'haskell', 'html', 'java', 'javascript', 'kotlin', 'lua', 'php', 'python', 'r', 'ruby', 'swift', 'typescript'];
+
+const App = () => (
+  <div className="app">
+    {languages.map(language => (
+      <ReactPll key={language} language={language} className="logo" />
+    ))}
+  </div>
+);
 
 ReactDOM.render(<App />, document.getElementById('root'));
